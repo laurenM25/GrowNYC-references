1eca12706100644bab352c61527a57624d8bb9c7
laurenM25 <lauren.marie.d25@gmail.com>
2025-05-22 01:10:38 -0400

    completely redoing everything (temporarily), removed image file upload and replaced with webscrapping methods

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 from waitress import serve
 from functions import names_and_photos, get_QR_filename, get_photo_filename, list_of_seeds, list_of_generics, list_of_companies
-from functions import update_database_list, save_user_input_img, BUCKET_NAME
+from functions import update_database_list, matches,locate_generic_specific_coords,retrieve_photo, save_feedback, BUCKET_NAME
 
 app = Flask(__name__)
+construction = True
 
 @app.route('/')
 @app.route('/homepage')
@@ -14,30 +15,29 @@ def index():
 def seed_info_page():
     seed_type = request.args.get('seed-type').lower()
 
-    matches = []
-    file_names = []
-    with open('static/seedList.txt', 'r') as file:
-        for line in file:
-            if line.split(":")[0] in seed_type:
-                if len(line.split(":")) > 1:
-                    varieties = line.split(":")[1]
-                    varieties = varieties.split(",")
-
-                    for variety in varieties:
-                        item = variety.strip() + " " + line.split(":")[0].strip()
-                        matches.append(item)
-                else:
-                    matches.append(line.strip())
-   
-    photo_urls = names_and_photos(matches)  
+    mchs = matches(seed_type)
+    if mchs is None:
+        title = f"No matches found for {seed_type}"
+        message = "Please check your spelling or look for alternate names your plant may have. Refer to the drop-down for available choices."
+        return render_template("error.html",error=title,expl=message)
+    if isinstance(mchs,tuple):
+        if len(mchs) == 2:
+            specifics, one_ornamental = mchs
+            photo_urls = names_and_photos(specifics, one_ornamental)
+        elif len(mchs) == 3:
+            all_specifics, all_generic_ornamentals, len_specifics = mchs
+            photo_urls = names_and_photos(all_specifics, all_generic_ornamentals, len_specifics)
+    else:
+        photo_urls = names_and_photos(mchs)  
 
     return render_template('seed-info-page.html', seed=seed_type.capitalize(), matches = photo_urls, companies=list_of_companies())
 
 @app.route('/pdf-viewer')
 def pdf_viewer():
     variety = request.args.get('variety').title() 
-    filename = get_photo_filename(variety)
-    photo_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/icons/{filename}"
+    g_i, s_i = locate_generic_specific_coords(variety)
+    print("coords: ", g_i, s_i)
+    photo_url = retrieve_photo(g_i, s_i, "plant-photo-url")
 
     company = request.args.get('company')
     date = request.args.get('month') + " " +  request.args.get("year")
@@ -46,6 +46,10 @@ def pdf_viewer():
 
 @app.route('/add-seed')
 def add_seed_page():
+    if construction == True:
+        title = "Work in Progress"
+        message = "Currently, this website does not allow for user input. The current model is just webscrapped information. Future work TBD."
+        return render_template('error.html',error=title,expl=message)
     return render_template('add-seed.html',seeds=list_of_seeds(), generics=list_of_generics(), companies=list_of_companies())
 
 @app.route('/confirm-new-entry', methods=['POST'])
@@ -74,6 +78,16 @@ def confirm_entry_page():
 
     #render template
     return render_template('confirm-new-entry.html',seed_name=seed_name)
+
+@app.route('/give-feedback')
+def give_feedback():
+    return render_template('give-feedback.html')
+
+@app.route('/confirm-feedback')
+def confirm_feedback():
+    feedback = request.args.get('feedback')
+    save_feedback(feedback)
+    return render_template('confirm-feedback.html')
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", port = 8000)
--- functions.py
+++ functions.py
@@ -5,6 +5,11 @@ import qrcode
 import os
 from datetime import datetime, timezone, timedelta
 import requests
+import pandas as pd
+import warnings
+
+# Suppress FutureWarning messages
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 #checking time drift - DEBUGGING
 # Local server time
@@ -28,7 +33,7 @@ print(session.get_credentials().get_frozen_credentials())
 s3 = session.client('s3')
 BUCKET_NAME = 'grownyc-app-assets'
 
-def generate_presigned_url(key, content_type, expiration=3600): #use presigned url 
+def generate_presigned_url(key, content_type, expiration=3600): #Not using - use presigned url 
     return s3.generate_presigned_url(
         'put_object',
         Params={
@@ -39,20 +44,123 @@ def generate_presigned_url(key, content_type, expiration=3600): #use presigned u
         ExpiresIn=expiration
     )
 
-def names_and_photos(matches):
-    my_dict = {}
+def matches(seed_type, find_index_generic = False): #checking matches between user input and list 
+    ornamental_generic = check_if_ornamental(seed_type)
 
-    base = f"https://{BUCKET_NAME}.s3.us-east-1.amazonaws.com/icons/"
-    for match in matches:
-        key = match.strip().replace(" ", "-")
-        my_dict[match] = [
-        base + key + "-QR.png",
-        base + key + ".jpg"
-        ]
+    if isinstance(ornamental_generic,list):
+        print("showing results for all ornamentals")
+        all_specifics = []
+        len_specifics = []
+        for generic in ornamental_generic:
+            all_specifics.extend(list_of_specifics(generic))
+            len_specifics.append(len(list_of_specifics(generic)))
+        return (all_specifics, ornamental_generic, len_specifics)
+    elif ornamental_generic not in "" and isinstance(ornamental_generic,str):
+        print(f"{seed_type} is ornamental, called '{ornamental_generic}'")
+        if find_index_generic == True:
+            return ornamental_generic, index_of_generic(ornamental_generic,use_full_name=True)
+        return (list_of_specifics(ornamental_generic),ornamental_generic) #also return generic name
     
+    #otherwise, check regular list of generics
+    print("not considered ornamental")
+    for generic in list_of_generics():
+        if generic.lower() in seed_type.lower():
+            #success
+            if find_index_generic == True:
+                return generic, index_of_generic(generic)
+            return list_of_specifics(generic)
+    else:
+        print(f"Could not find the generic seed variety in the inputted name '{seed_type}'")
+        #check for keyword in literally any of the varieties
+        if desperate_match_check(seed_type) != []:
+            print("Desperate match successful")
+            print(desperate_match_check(seed_type))
+            return desperate_match_check(seed_type)
+        print("Desperate match check unsuccessful. Returning None.")
+        return None
+
+def desperate_match_check(keyword): #check for keyword in literally any of the varieties
+    desperate_list = []
+    for variety in list_of_seeds():
+        print(f"comparing {keyword} with {variety}")
+        if keyword.lower().strip() in variety.lower():
+            print(f"yes, {keyword} is in '{variety}")
+            desperate_list.append(variety)
+    return desperate_list
+
+def check_if_ornamental(seed):
+    all_ornamentals = ["Kale, Ornamental", "Grasses, Ornamental", "Cress, Ornamental", "Basil, Ornamental"]
+    if "ornamental" in seed.lower():
+        print("yup, it's ornamental")
+        ornamentals = ["kale", "grasses", "cress", "basil"]
+        return next((f"{orn_type.capitalize()}, Ornamental" for orn_type in ornamentals if orn_type in seed.lower()), all_ornamentals) 
+    else:
+        print(f"nope, {seed} is not ornamental")
+        return ""
+
+def index_of_generic(string, use_full_name = False):
+    for i,generic in enumerate(list_of_generics()):
+        if use_full_name == True:
+            if generic.lower() == string.lower(): #only accept an exact match
+                return i
+        elif generic.lower() in string.lower():
+            return i
+        
+def index_of_specific(generic, specific):
+    for i,string in enumerate(list_of_specifics(generic)):
+        if string.lower() in specific.lower():
+            return i
+        
+    print(f"'{string}' not found in the specific varieties of '{generic}'")
+        
+def locate_generic_specific_coords(string): #from just its full name -- returns (generic, specific)
+    generic, generic_index = matches(string, find_index_generic=True)
+    print("Line 74:", generic)
+    specific_index = index_of_specific(generic, string)
+
+    print("indices: ", generic_index, "and", specific_index)
+    return generic_index,specific_index
+    
+def names_and_photos(matches, specified_generic = None, len_multi_speci_lists = None): # -> {"plant-name": [QR, photo]}
+    my_dict = {}
+    if matches == []:
+        print("no matches, so returning None for photos")
+        return None
+    print("names_and_photos() def, type:",type(matches)) 
+    print(matches)
+    if not isinstance(matches, list):
+        print("Incorrect: the matches variable must be type list.")
+        return
+
+    if specified_generic is not None:
+        if isinstance(specified_generic,list): #if I have multiple generic varieties, get multiple indices
+            generic_index = [index_of_generic(gen1,use_full_name=True) for gen1 in specified_generic]
+            #special multi indicies, iterate through matches paying attential to various indicies
+            specific_index = 0
+            threshold = 0
+            for i,match in enumerate(matches):
+                #obtain QR and photo
+                if specific_index >= len_multi_speci_lists[threshold]:
+                    threshold += 1
+                    specific_index = 0
+                g_index = generic_index[threshold]
+                my_dict[match] = [retrieve_photo(g_index,specific_index,"QR-url"),retrieve_photo(g_index,specific_index,"plant-photo-url")]
+                specific_index += 1
+            return my_dict
+        else:
+            generic_index = index_of_generic(specified_generic,use_full_name=True)
+    else:
+        generic_index = index_of_generic(matches[0])
+
+    #just one generic index
+    specific_index = 0
+    for match in matches:
+        #obtain QR and photo
+        my_dict[match] = [retrieve_photo(generic_index,specific_index,"QR-url"),retrieve_photo(generic_index,specific_index,"plant-photo-url")]
+        specific_index += 1
     return my_dict
 
-def get_QR_filename(variety_name): #input a string, output a string
+def get_QR_filename(variety_name): #not in use rn - input a string, output a string
     variety_name = variety_name.strip().lower()
     return variety_name.replace(" ", "-") + "-QR.png"
 
@@ -103,38 +211,55 @@ def get_photo_filename(variety_name):
     variety_name = variety_name.strip().lower()
     return variety_name.replace(" ", "-") + ".jpg"
 
-def list_of_seeds(): #for the drop-down list on homepage
+def list_of_seeds(): #for the drop-down list on homepage, all seed varieties
     seeds = []
 
-    with open('static/seedList.txt', 'r') as file:
-        for line in file:
-            if len(line.split(":")) > 1:
-                varieties = line.split(":")[1]
-                varieties = varieties.split(",")
-
-                for variety in varieties:
-                    item = variety.strip() + " " + line.split(":")[0]
-                    seeds.append(item)
-            else:
-                seeds.append(line)
+    for generic in list_of_generics():
+        specifics = list_of_specifics(generic)
+        seeds.extend(specifics)
 
     return seeds
 
 def list_of_generics():
-    seeds = []
+    seed_df = add_scraped_to_seed_list()
+    return seed_df["name"]
 
-    with open('static/seedList.txt', 'r') as file:
-        for line in file:
-            seeds.append(line.split(":")[0])
+def row_generic(generic):
+    seed_df = add_scraped_to_seed_list()
+    seed_df = seed_df.set_index("name")
+    return seed_df.loc[generic]
 
-    return seeds
+def list_of_specifics(generic):
+    row = row_generic(generic)
+    l = []
+    for i,dict in enumerate(row["specific-varieties"]):
+        if isinstance(dict, list):
+            dict = dict[0]
+        variety = dict["variety-name"].replace(generic,"").strip()
+        variety = variety + " " + generic
+        l.append(variety)
+    return l
 
 def list_of_companies():
     companies = ["Johnny's Seeds", "Hudson Valley Seed"]
 
     return companies
 
-def update_database_list(generic, specific, company, QR_link, image): 
+def save_feedback(str):
+    with open("static/feedback.txt", 'w') as f:
+        f.write(str)
+    with open("static/feedback.txt", 'rb') as f:
+        s3_key = f'feedback/feedback-{datetime.now(timezone.utc)}.txt'
+        presigned_url = generate_presigned_url(s3_key, 'text/plain')
+        response = requests.put(
+            presigned_url,
+            data=f,
+            headers={'Content-Type': 'text/plain'}
+        )
+        if response.status_code != 200:
+            raise Exception(f"Failed to upload text file: {response.text}")
+
+def update_database_list(generic, specific, company, QR_link, image): #not in use
     name = specific + " " + generic
     image_name = get_photo_filename(name)
     QR_name = get_QR_filename(name)
@@ -173,7 +298,7 @@ def update_database_list(generic, specific, company, QR_link, image):
 
     #DEAL WITH COMPANY LATER --> johnny default, but if Hudson, need that to reflect in filename
 
-def save_user_input_img(filepath, file):
+def save_user_input_img(filepath, file): #not in use
 
     if file:
         filename = secure_filename(filepath)
@@ -189,7 +314,7 @@ def save_user_input_img(filepath, file):
         return f"https://{BUCKET_NAME}.s3.us-east-1.amazonaws.com/icons/{filename}"
     return None
 
-def upload_txt_to_s3(local_path): #update txt file in S3 bucket
+def upload_txt_to_s3(local_path): # not in use. update txt file in S3 bucket
     with open(local_path, 'rb') as f:
         s3_key = 'seedList.txt'
         presigned_url = generate_presigned_url(s3_key, 'text/plain')
@@ -200,3 +325,34 @@ def upload_txt_to_s3(local_path): #update txt file in S3 bucket
         )
         if response.status_code != 200:
             raise Exception(f"Failed to upload text file: {response.text}")
+
+def retrieve_photo(generic_i,specific_i,photo_type):
+    if photo_type != "QR-url" and photo_type != "plant-photo-url":
+        return f"Error retrieving photo: Incorrect photo_type was specified. You said {photo_type} but it needs to be QR-url or plant-photo-url"
+    seed_df = add_scraped_to_seed_list()
+
+#QR code won't work right now. It is just a link.
+    return seed_df["specific-varieties"][generic_i][specific_i][photo_type]
+
+def add_scraped_to_seed_list():
+    # convert to table
+    seed_df = pd.read_csv("scraped_seeds.csv")
+    seed_df["name"] = seed_df["name"].str.replace(r'\s*\(.*?\)', '', regex=True)
+
+    seed_df["specific-varieties"] = [eval(variety) for variety in seed_df["specific-varieties"]]
+    #remove duplicate specifics
+    for i, row in enumerate(seed_df["specific-varieties"]):
+        running_list = []
+        list_dicts = []
+        for d in row:
+            d["variety-name"] = strip_punctuation(d["variety-name"],"'")
+            if d["variety-name"] not in running_list:
+                running_list.append(d["variety-name"])
+                list_dicts.append(d)
+        seed_df["specific-varieties"][i] = list_dicts
+    return seed_df
+
+def strip_punctuation(str, regex):
+    return str.strip(f"{regex}")
+#print(list_of_generics().to_list())
+#print(list_of_seeds())
